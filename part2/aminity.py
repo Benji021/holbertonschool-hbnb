3d7655fc79b5dd9fac5115d5ab49687dee989726
Benji021 <9548holbertonstudents.com>
2024-10-22 14:41:09 +0200

    Class Aminity

--- part2/aminity.py
+++ part2/aminity.py
@@ -3,16 +3,54 @@
 
 import uuid
 from datetime import datetime
+import re
+
 
 class aminity:
-    def __init__(self, id, name):
+    def __init__(self, id, name, description, available=True):
         self.id = str(uuid.uuid4())
         self.name = name
+        self.description = description
+        self.available = available
 
-        def save(self):
-            """Update the update_at timestamp whenever the object is modified"""
-            self.created_at = datetime.now()
-            self.update_at = datetime.now()
+    # Check name validity
+    def validity_name(name):
+        # Length check (between 3 an 30 characters)
+        if len(name) < 3 or len(name) > 30:
+            return False
+        
+        # Character verification: letters, spaces, apostrophes and hyphens are allowed
+        # Regex: ^ = start, $ = end, [a-zA-ZÀ-ÿ] = letters with accents
+        # Apostrophes and hyphens are accepted between letters.
+        if not re.match(r"^[a-zA-ZÀ-ÿ' -]+$", name):
+            return False
+        
+        return True
+    
+    # Check validity description to the place
+    def validity_description(description):
+        # Length check (between 3 an 500 characters)
+        if len(description) < 3 or len(description) > 500:
+            return False
+        
+        # Character verification: letters, spaces, apostrophes and hyphens are allowed
+        # Regex: ^ = start, $ = end, [a-zA-ZÀ-ÿ] = letters with accents
+        # Apostrophes and hyphens are accepted between letters.
+        if not re.match(r"^[a-zA-ZÀ-ÿ' -]+$", description):
+            return False
+        
+        return True
+    
+    # Display the details of the aminity
+    def __str__(self):
+        return f"{self.name}: {self.description} (Available: {self.available})"
+
+
+    # Validity datetime
+    def save(self):
+        """Update the update_at timestamp whenever the object is modified"""
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
 
     def update(self, data):
         """Update the attributes of the object based on the provided dictionary"""
