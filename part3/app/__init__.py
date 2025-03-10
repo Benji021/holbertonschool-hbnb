50d0ccafa338d0c22469e0cd8aa027f1c23d517f
Benji021 <9548holbertonstudents.com>
2025-03-10 15:21:55 +0100

    Implement Authenticated User Access Endpoints

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity # Import the JWT manager
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -7,6 +8,14 @@ from app.api.v1.reviews import api as reviews_ns
 
 def create_app():
     app = Flask(__name__)
+
+    # Configure secret key for JWT
+    app.config['JWT_SECRET_KEY'] = 'my_secret_key' # Replaces with a more secure key
+
+    # Initialize the JWT manager
+    jwt = JWTManager(app)
+
+    # Create API
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     # Register the users namespace
