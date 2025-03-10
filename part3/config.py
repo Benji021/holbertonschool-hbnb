5981a499726b8b49080ed709f053186f00367b81
Entwoane <antoine.musci@gmail.com>
2025-03-10 18:01:41 +0100

    1. Modify the User Model to Include Password Hashing

---
+++ part3/config.py
@@ -0,0 +1,16 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "hbnb_database.db")}'
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
