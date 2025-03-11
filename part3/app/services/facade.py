94065cd13f5a398c49ce91def9c811fa9063d81c
Benji021 <9548holbertonstudents.com>
2025-03-11 15:41:48 +0100

    Facade

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -7,6 +7,12 @@ from app.models.review import Review
 
 
 class HBnBFacade:
+    def authenticate_user(self, email, password):
+        user = User.query.filter_by(email=email).first()
+        if user and user.check_password(password):  # Vérifie le mot de passe
+            return user
+        return None
+
     def __init__(self):
         self.user_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
