18df5626cad45fd00aef859f3f1dbcd79110a8b1
Benji021 <9548holbertonstudents.com>
2025-03-12 13:55:32 +0100

    init

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,73 +1,31 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_restx import Api
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity # Import the JWT manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-from app.services import HBnBFacade
 
-facade = HBnBFacade()
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
-
-    app.config["DEBUG"] = True  # Activate debug mode
-
-    # Configure secret key for JWT
-    app.config['JWT_SECRET_KEY'] = 'my_secret_key' # Replaces with a more secure key
-
-    # Initialize the JWT manager
-    jwt = JWTManager(app)
-
-    # JWT error handling
-    @jwt.expired_token_loader
-    def expired_token_callback(jwt_header, jwt_payload):
-        return jsonify({"msg": "Le token a expiré"}), 401
-
-    @jwt.invalid_token_loader
-    def invalid_token_callback(error):
-        return jsonify({"msg": "Token invalide"}), 401
-
-    @jwt.unauthorized_loader
-    def missing_token_callback(error):
-        return jsonify({"msg": "Token manquant"}), 401
-
-    # Create API
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hbnb_database.db'
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    app.config.from_object(config_class)
 
+    db.init_app(app)
+    bcrypt.init_app(app)
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
-    # Create a connection endpoint
-    @app.route('/login', methods=['POST'])
-    def login():
-
-        # Retrieve user credentials sent in the request body
-        email = request.json.get('email', None)
-        password = request.json.get('password', None)
-
-        print(f"DEBUG - Email: {email}, Password: {password}")  # Voir les valeurs
-        print(f"DEBUG - facade: {facade}")  # Vérifier si `facade` est bien créé
-        print(f"DEBUG - Has authenticate_user? {hasattr(facade, 'authenticate_user')}")
-
-        # Check identifiers (replace this logic with a real check in your DB)
-        user = facade.authenticate_user(email, password)
-
-        if not user:
-            return jsonify({"msg": "Identifiants invalides"}), 401
-
-        # Use user ID as identity in token
-        access_token = create_access_token(identity=user.id)
-
-        return jsonify(access_token=access_token), 200
-
-
     return app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, host="0.0.0.0", port=5000)  
+    app.run(debug=True, host="0.0.0.0", port=5000) 
