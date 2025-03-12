54e12a55f0e540f3c2efec09aeaa598348e61b51
Benji021 <9548holbertonstudents.com>
2025-03-12 15:26:57 +0100

    Init

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -7,6 +7,7 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import auth_bp # import auth blueprint
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -17,6 +18,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hbnb_database.db'
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
     app.config.from_object(config_class)
+    app.register_blueprint(auth_bp) # Register the auth blueprint
 
     db.init_app(app)
     bcrypt.init_app(app)
