ea3734675c59936bec466782a5c4bc5edfc2f4ea
Benji021 <9548holbertonstudents.com>
2025-03-12 14:06:36 +0100

    Init

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -9,6 +10,7 @@ from app.api.v1.reviews import api as reviews_ns
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
