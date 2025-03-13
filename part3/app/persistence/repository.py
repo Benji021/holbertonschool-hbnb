66996965b912caa3dd11eec667ed630dea31d349
Benji021 <9548holbertonstudents.com>
2025-03-13 15:16:48 +0100

    Repository

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+
 class Repository(ABC):
     @abstractmethod
     def add(self, obj):
@@ -27,77 +28,25 @@ class Repository(ABC):
 
 class InMemoryRepository(Repository):
     def __init__(self):
-        self._data = {}
-
-    def save(self, amenity):
-        """Saves an updated amenity in memory storage"""
-        self._data[amenity.id] = amenity  # Simulates backup
+        self._storage = {}
 
     def add(self, obj):
-        self._data[obj.id] = obj
+        self._storage[obj.id] = obj
 
     def get(self, obj_id):
-        return self._data.get(obj_id)
+        return self._storage.get(obj_id)
 
     def get_all(self):
-        return list(self._data.values())
+        return list(self._storage.values())
 
-    def update(self, amenity_id, data):
-        print(f">>> Type de data reçu dans update(): {type(data)} - Contenu: {data}")
-
-        if not isinstance(data, dict):
-            raise TypeError(f"Expected 'data' to be a dict, got {type(data)} instead.")
-    
-        amenity = self.get(amenity_id)
-        if not amenity:
-            return None
-
-        for key, value in data.items():
-            setattr(amenity, key, value)
-
-        if hasattr(self, "save"):
-            self.save(amenity)
-
-        return amenity
+    def update(self, obj_id, data):
+        obj = self.get(obj_id)
+        if obj:
+            obj.update(data)
 
     def delete(self, obj_id):
-        if obj_id in self._data:
-            del self._data[obj_id]
+        if obj_id in self._storage:
+            del self._storage[obj_id]
 
     def get_by_attribute(self, attr_name, attr_value):
-        return next((obj for obj in self._data.values() if getattr(obj, attr_name) == attr_value), None)
-
-from app import db  # Assuming you have set up SQLAlchemy in your Flask app   
-from app.models import User, Place, Review, Amenity  # Import your models
-class SQLAlchemyRepository(Repository):
-        def __init__(self, model):
-            self.model = model
-
-        def add(self, obj):
-            db.session.add(obj)
-            db.session.commit()
-
-        def get(self, obj_id):
-            return self.model.query.get(obj_id)
-
-        def get_all(self):
-            return self.model.query.all()
-
-        def update(self, obj_id, data):
-            obj = self.get(obj_id)
-            if obj:
-                for key, value in data.items():
-                    setattr(obj, key, value)
-                db.session.commit()
-                return obj # Return object with updated
-
-        def delete(self, obj_id):
-            obj = self.get(obj_id)
-            if obj:
-                db.session.delete(obj)
-                db.session.commit()
-
-        def get_by_attribute(self, attr_name, attr_value):
-           if hasattr(self.model, attr_name): # Checking to avoid an exception
-                return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
-           return None
+        return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
