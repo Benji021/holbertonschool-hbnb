c07b89d34ece9f9be2fc06b673a68e56906add46
Benji021 <9548holbertonstudents.com>
2025-03-13 16:00:21 +0100

    Users

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -1,5 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
+from flask_bcrypt import generate_password_hash
 
 api = Namespace('users', description='User operations')
 
@@ -7,7 +8,8 @@ api = Namespace('users', description='User operations')
 user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
+    'email': fields.String(required=True, description='Email of the user'),
+    'password': fields.String(required=True, description='Password of the user')
 })
 
 @api.route('/')
@@ -26,6 +28,9 @@ class UserList(Resource):
             return {'error': 'Email already registered'}, 400
 
         try:
+            # Hash the password before storing
+            user_data['password'] = generate_password_hash(user_data['password']).decode('utf-8')
+            
             new_user = facade.create_user(user_data)
             return new_user.to_dict(), 201
         except Exception as e:
