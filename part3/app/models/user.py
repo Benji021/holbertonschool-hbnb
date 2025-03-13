6afd27db49599baaa9178229595302b0f6051d7f
Benji021 <9548holbertonstudents.com>
2025-03-13 16:11:59 +0100

    Model user

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Defining the user class, its attributes and relationships """
 
-from .basemodel import BaseModel
+from app.models.user import BaseModel
 from app import bcrypt
 import re
 
