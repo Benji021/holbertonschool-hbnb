70f501961a5b62baf2536f1a619511daf9f56901
Benji021 <9548holbertonstudents.com>
2025-03-13 15:15:08 +0100

    Model Place

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -2,117 +2,110 @@
 """ Defining the place class, its attributes and relationships """
 
 
-import uuid
-from datetime import datetime
-from app.models.review import Review
+from .basemodel import BaseModel
+from .user import User
 
-class Place:
-    def __init__(self, id,  title, price, latitude, longitude, owner_id, description=None):
-        """ Initialization of a location with input validation """
-        super().__init__() # Initialize parent class
+class Place(BaseModel):
+    def __init__(self, title: str, price: float, latitude: str, longitude, owner, description=None):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.reviews = []  # List to store related reviews
+        self.amenities = []  # List to store related amenities
 
-        # Generate a unique ID
-        self.id = str(uuid.uuid4())
+    @property
+    def title(self):
+        return self.__title
+    
+    @title.setter
+    def title(self, value):
+        if not value:
+            raise ValueError("Title cannot be empty")
+        if not isinstance(value, str):
+            raise TypeError("Title must be a string")
+        super().is_max_length('title', value, 100)
+        self.__title = value
 
-        # Field Check
-        self.title = self.validate_title(title)
-        self.description = description if description else ""
-        self.price = self.validate_price(price)
-        self.latitude = self.validate_latitude(latitude)
-        self.longitude = self.validate_longitude(longitude)
-        self.owner = self.validate_owner(owner_id)
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    @property
+    def price(self):
+        return self.__price
+    
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, float) and not isinstance(value, int):
+            raise TypeError("Price must be a float")
+        if value < 0:
+            raise ValueError("Price must be positive.")
+        self.__price = value
 
-        self.owner = self.get_user(owner_id)  # Retrieves the user with this ID
-        if not self.owner:
-            raise ValueError(f"Invalid owner_id: No user found with ID {owner_id}")
+    @property
+    def latitude(self):
+        return self.__latitude
+    
+    @latitude.setter
+    def latitude(self, value):
+        if not isinstance(value, float):
+            raise TypeError("Latitude must be a float")
+        super().is_between("latitude", value, -90, 90)
+        self.__latitude = value
+    
+    @property
+    def longitude(self):
+        return self.__longitude
+    
+    @longitude.setter
+    def longitude(self, value):
+        if not isinstance(value, float):
+            raise TypeError("Longitude must be a float")
+        super().is_between("longitude", value, -180, 180)
+        self.__longitude = value
 
-        # Relation
-        self.amenities = [] # Equipment list
-        self.reviews = [] # List of reviews associated with this location
-
-        self.owner.add_place(self)
-
-    def get_user(self, owner_id):
-        """ Convertit owner_id en un objet User si c'est un ID """
-        from app.models.user import User # Import here to avoid circular import
-        if isinstance(owner_id, User):  # If it's already a User, we keep it
-            return owner_id
-        elif isinstance(owner_id, str): # If it's an ID, we look for the User object
-            return self.find_user_by_id(owner_id)
-        return None
-
-    @staticmethod
-    def find_user_by_id(user_id):
-        """ Simule la récupération d'un utilisateur par son ID (à remplacer par une requête DB) """
-        from app.models.user import User # Import here to avoid circular import
-        # Fictitious example (replace this part with your ORM query)
-        users_db = {
-            "1234": User("Alice", "Dupont", "alice@example.com"),
-            "5678": User("Bob", "Martin", "bob@example.com")
-        }
-        return users_db.get(user_id, None)  # Returns None if ID does not exist
-
-    def get_owner_object(self, owner_id):
-        """ Retrieves the User object associated with owner_id """
-        from app.models.user import User # Import here to avoid circular import
-        user = User.get(owner_id)  # Recover user ID
-        if not user:
-            raise ValueError(f"Aucun utilisateur trouvé avec l'ID {owner_id}")
-        return user
-
-    def add_aminity(self, aminity):
-        """ Associate a piece of equipment with this location """
-        if isinstance(aminity, aminity):
-            self.amenities.append(aminity)
-        else:
-            raise TypeError("The object added must be an instance of Amenity")
+    @property
+    def owner(self):
+        return self.__owner
+    
+    @owner.setter
+    def owner(self, value):
+        if not isinstance(value, User):
+            raise TypeError("Owner must be a user instance")
+        self.__owner = value
 
     def add_review(self, review):
-        """ Associates a notice with this location """
-        if isinstance(review, Review):
-            self.reviews.append(review)
-            review.place = self # Associate notice with this location
-        else:
-            raise TypeError("The object added must be an instance of Review")
+        """Add a review to the place."""
+        self.reviews.append(review)
+    
+    def delete_review(self, review):
+        """Add an amenity to the place."""
+        self.reviews.remove(review)
 
-    def validate_title(self, title):
-        """ Validate Title """
-        if not title or len(title) > 100:
-            raise ValueError("The title is mandatory and must not exceed 100 characters.")
-        return title
+    def add_amenity(self, amenity):
+        """Add an amenity to the place."""
+        self.amenities.append(amenity)
 
-    def validate_price(self, price):
-        """ Validate Price """
-        if price < 0:
-            raise ValueError("The price must be a positive value.")
-        return price
-
-    def validate_latitude(self, latitude):
-        """ Validate Latitude """
-        if not (-90.0 <= latitude <= 90.0):
-            raise ValueError("Latitude must be between -90.0 and 90.0.")
-        return latitude
-
-    def validate_longitude(self, longitude):
-        """ Validate Longitude """
-        if not (-180.0 <= longitude <= 180.0):
-            raise ValueError("Longitude must be between -180.0 and 180.0")
-        return longitude
-
-    def validate_owner(self, owner):
-        """ Validates the owner's existence """
-        if not owner:
-            raise ValueError("A valid owner is required.")
-        return owner
-
-    def update(self, **kwargs):
-        """ Updates location attributes and timestamp """
-        for key, value in kwargs.items():
-            if hasattr(self, key) and key != "id" and key != "created_at":
-                setattr(self, key, value)
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        return f"{self.title} - {self.price}€/night, {self.latitude}, {self.longitude} (Owner: {self.owner})"
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'price': self.price,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'owner_id': self.owner.id
+        }
+    
+    def to_dict_list(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'price': self.price,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'owner': self.owner.to_dict(),
+            'amenities': self.amenities,
+            'reviews': self.reviews
+        }
