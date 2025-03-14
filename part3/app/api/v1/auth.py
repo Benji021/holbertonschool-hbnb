b0c71d0c8e4a82d187d7c41944d6955b4689eac7
Benji021 <9548holbertonstudents.com>
2025-03-14 10:49:01 +0100

    Authentification

--- part3/app/api/v1/auth.py
+++ part3/app/api/v1/auth.py
@@ -1,9 +1,7 @@
-from flask import Blueprint, request, jsonify
 from flask_restx import Namespace, Resource, fields
-from flask_jwt_extended import create_access_token, JWTManager
+from flask_jwt_extended import create_access_token
 from app.services import facade
 
-auth_bp = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
 api = Namespace('auth', description='Authentication operations')
 
 # Model for input validation
