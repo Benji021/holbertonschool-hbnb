0782151062e36385ba234c06ad639e87336c6d14
Benji021 <9548holbertonstudents.com>
2025-04-08 16:06:06 +0200

    Scripts JS

--- part4/static/js/scripts.js
+++ part4/static/js/scripts.js
@@ -1,8 +1,3 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
     // Simulates data retrieved from an API
     const place = {
