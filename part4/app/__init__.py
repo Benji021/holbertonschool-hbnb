69c1cb0f1ae0c5d099f743948548f7baa4c8c92f
Benji021 <9548holbertonstudents.com>
2025-04-10 16:47:06 +0200

    init

--- part4/app/__init__.py
+++ part4/app/__init__.py
@@ -10,9 +10,20 @@ from app.extensions import bcrypt, jwt, db
 from app.database import init_db, seed_db
 
 def create_app(config_class="config.DevelopmentConfig"):
-    app = Flask(__name__, 
-            template_folder='/root/holbertonschool-hbnb/part4/templates',
-            static_folder='/root/holbertonschool-hbnb/part4/static')
+    # Define path file Templates and Static
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    TEMPLATE_DIR = os.path.join(BASE_DIR, '..', 'templates')
+    STATIC_DIR = os.path.join(BASE_DIR, '..', 'static')
+
+    # Déboguer les chemins
+    print("TEMPLATE_DIR:", TEMPLATE_DIR)  # Affiche le chemin pour vérifier si c'est correct
+    print("STATIC_DIR:", STATIC_DIR)  # Affiche le chemin des fichiers statiques
+
+    # Déboguer les fichiers dans templates
+    print("Files in TEMPLATE_DIR:", os.listdir(TEMPLATE_DIR))
+
+    # Create the Flask application, specifying folders for templates and static files
+    app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/swagger')
     bcrypt.init_app(app=app)
